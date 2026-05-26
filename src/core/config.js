173cf5bf2a8563cadd79bc3fe8c6b8f56891f2a3
AiSatan <2905049+AiSatan@users.noreply.github.com>
2026-05-26 20:48:21 +0700

    purgee

--- src/core/config.js
+++ src/core/config.js
@@ -6,7 +6,7 @@
     wikiLookupConcurrency: 2,
     fallbackIcon: "https://www.poewiki.net/images/b/b6/Scroll_of_Wisdom_inventory_icon.png",
     cache: {
-      namespace: "PoE2DireCache-25-05-2026:",
+      namespace: "PoE2DireCache-26-05-2026",
       // 4 months
       hitTtlMs: 120 * 24 * 60 * 60 * 1000,
       // 7 days
